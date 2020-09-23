0c37009b5fdd49f16f8157a47ef4df26c51e80f9
Joi <37125335+joicodes@users.noreply.github.com>
2020-09-23 09:07:18 -0400

    created a function for each command

--- respond.py
+++ respond.py
@@ -5,19 +5,31 @@ import time
 # Global Constants
 today = datetime.now()
 
+# Commands for JoiBot 
+def get_time():
+  now = datetime.now()
+  return now.strftime("%I:%M %p")
+
+def get_date():
+  return today.strftime("%A, %B %-d, %Y")
+
+def get_weekday():
+  return today.strftime('%A') 
+
+
+# Returns a response based on voice data
 def respond(voice_data):
   if 'your name' in voice_data:
     return 'My name is JoiBot 🤖'
 
   elif 'time' in voice_data:
-    now = datetime.now()
-    return now.strftime("%I:%M %p")
+    return get_time()
 
   elif "date" in voice_data:
-    return today.strftime("%A, %B %-d, %Y")
+    return get_date()
 
   elif "day of the week" in voice_data:
-    return today.strftime('%A')
+    return get_weekday()
 
   else:
     return "Sorry. I can't answer that question."
