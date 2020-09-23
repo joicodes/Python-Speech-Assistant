f49b94e081c5ef006ef167d9d0d2e67fe1d39c89
Joi <37125335+joicodes@users.noreply.github.com>
2020-09-23 01:00:20 -0400

    Create record.py

---
+++ record.py
@@ -0,0 +1,41 @@
+import speech_recognition as sr
+from print_color import print_cyan, print_red
+
+# Initialize a recognizer
+recognizer = sr.Recognizer()
+
+def record_user_audio(device_index, sample_rate):
+    
+  with sr.Microphone(device_index=device_index, sample_rate=sample_rate) as source:
+
+    attempts = 0
+
+    while attempts <= 2: 
+
+      voice_data = None
+
+      try:
+          # Records a single phrase from source.
+          audio = recognizer.listen(source, timeout=5)
+          
+          # Recognize speech using Google Speech Recognition API.
+          voice_data = recognizer.recognize_google(audio)
+
+      except (sr.WaitTimeoutError, sr.UnknownValueError):
+
+        if attempts < 2:
+          print("Sorry, I didn't get that. 😅")
+          print_cyan("Ask me again...\n")
+          attempts += 1
+          continue
+
+        else:
+          print("I still can't hear you. 😓")
+          print_red("Try again later.\n") 
+
+      except sr.RequestError:
+        print("😓 Well, this is awkward. My speech services is not working.")
+        print_red("Try again later.")
+        break
+
+      return voice_data
