e8a03ecb3907ab72a0ac6326cb840f650be0f199
Joi <37125335+joicodes@users.noreply.github.com>
2020-09-23 09:42:06 -0400

    Added speak functionality

--- speak.py
+++ speak.py
@@ -1,5 +1,28 @@
-import gTTs
-import playsound
+from gtts import gTTS
+import os
+import random
+from playsound import playsound
 
-# Methods for speech synthesis using Google Text to Speech
 
+
+def create_audio_file_name():
+    num = random.randint(1, 10000)
+    audio_file_name = f'joibot-audio-{num}.mp3'
+    return audio_file_name
+
+def remove_audio_file(audio_file):
+    os.remove(audio_file)
+
+def speak(answer):
+    # Speech synthesis using Google Text to Speech
+    tts = gTTS(text=answer, lang='en')
+
+    # Generates a audiofile name and saves tts
+    audio_file = create_audio_file_name()
+    tts.save(audio_file)
+
+    # Plays the audio file
+    playsound(audio_file)
+
+    # Returns the name of the file
+    return audio_file
