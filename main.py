4ab2d1c29f8cd9b7df54d8b1c2b520286a2deb45
Joi <37125335+joicodes@users.noreply.github.com>
2020-09-22 23:37:44 -0400

    uses helper functions

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import speech_recognition as sr
 import pyaudio
-from colorama import init, Fore, Style
+from print_color import *
+from print_speed import *
 from time import sleep
 from datetime import datetime
-import sys
-
-# Initialize Colorama
-init(autoreset=True)
+import webbrowser
 
 # Initialize a recognizer
 recognizer = sr.Recognizer()
@@ -22,12 +20,6 @@ device_sample_rate = int(pa.get_default_input_device_info()['defaultSampleRate']
 
 # Global Constants
 today = datetime.now()
-
-def delay_print(s):
-    for c in s:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        sleep(0.03)
 
 
 def record_user_audio():
@@ -74,44 +66,19 @@ def respond(voice_data):
 
 def main():
 
-  # Print statement to prompt user to say something. 
   print("\nHi, I am JoiBot! 👋")
   print("You're virtual assistant.")
 
-  #print(f"\nSpeak to me using your {device_name}.")
-  print(Fore.CYAN+"Go ahead. Ask me a question...\n")
+  print(f"\nSpeak to me using your {device_name}.")
+  print_cyan("Go ahead. Ask me a question...\n")
   
   voice_data = record_user_audio()
 
   if voice_data != '' or voice_data != None: 
-    delay_print(f'{voice_data.capitalize()}?\n')
+    print_slow(f'{voice_data.capitalize()}?\n')
     bot_response = respond(voice_data)
     sleep(0.3)
-    print(Fore.GREEN+bot_response)
+    print_green(bot_response)
     sleep(0.5)
 
 main()
-
-
-
-
-
-
-
-
-
-
-          # print(f'If correct, say "Yes". If not, say "No"')
-
-          # confirm_audio = recognizer.listen(source, timeout=30)
-          # response = recognizer.recognize_google(confirm_audio)
-
-          # # If "yes", print confirmation greeting and exit.
-          # if response == "yes":
-          #     print("\nNice. I heard you loud and clear! 😄")
-              
-          # # If "no", print apology and loops again to try again.
-          # elif response == "no":
-          #     print("\nSorry that I did not hear you. 😓\n Let's try again! ")
-          # else:
-          #     print("\nThat was not a valid response. Goodbye!👋")
