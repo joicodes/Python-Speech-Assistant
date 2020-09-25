d244271937968902082f45c7293382af6f0ae5db
Joi <37125335+joicodes@users.noreply.github.com>
2020-09-25 11:57:26 -0400

    added more commands

--- respond.py
+++ respond.py
@@ -3,11 +3,22 @@ from random import randint, choice
 import webbrowser
 import time
 
-# Global Constants
 today = datetime.now()
+
 
 # Commands for JoiBot 
 
+# About JoiBot
+def get_name():
+  return 'My name is JoiBot.'
+
+def get_hometown():
+  return 'I am from the Big Apple.'
+
+def get_creator():
+  return 'I was created by Joi Anderson.'
+
+# Alarms, Timer & Clock Commands
 def get_time():
   now = datetime.now()
   return now.strftime("%I:%M %p")
@@ -18,7 +29,23 @@ def get_date():
 def get_weekday():
   return today.strftime('%A') 
 
-# For Fun
+# TO DO: Web Search Commands
+
+def search_google():
+  pass
+
+def search_google_img():
+  pass
+def search_youtube():
+  pass
+
+def search_wikipedia():
+  pass
+
+def search_google_maps():
+  pass
+
+# For Fun  
 def get_coin_toss():
   flip = randint(0, 1)
   return "Head." if flip else "Tails."
@@ -36,11 +63,20 @@ def get_card():
 
 # Returns a response based on voice data
 def respond(voice_data):
+
+  voice_data = voice_data.lower()
+
   if 'bye' in voice_data:
     return 'Nice talking to you! Goodbye.'
   
   elif 'your name' in voice_data:
-    return 'My name is JoiBot.'
+    return get_name()
+
+  elif 'where are you from' in voice_data:
+    return get_hometown()
+
+  elif 'created' in voice_data or 'creator' in voice_data:
+    return get_creator()
 
   elif 'time' in voice_data:
     return get_time()
@@ -50,6 +86,19 @@ def respond(voice_data):
 
   elif "day of the week" in voice_data:
     return get_weekday()
+
+  elif "search" in voice_data:
+    if "video" in voice_data:
+      return search_youtube()
+    elif "photo" in voice_data:
+      return search_google_img()
+    elif "wikipedia" in voice_data:
+      return search_wikipedia()
+    else:
+      return search_google()
+
+  elif "where is" in voice_data:
+    return search_google_maps()
 
   elif "flip a coin" in voice_data:
     return get_coin_toss()
